9ea7e83bcf16d9f978d93c0c8794058ce67129b6
Miguel Jones <mjjones@email.wm.edu>
2025-04-08 00:41:03 -0400

    Added tailwind styling to the project

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -28,31 +28,51 @@ function App() {
 
   return (
     <>
-      <h1>All Cat Facts</h1>
-      <ul>
-        <table>
-          <thead>
-            <tr>
-              <th><label>ID</label></th>
-              <th><label>Fact</label></th>
-              <th><label>Created At</label></th>
-            </tr>
-          </thead>
+      <div class="min-h-screen flex flex-col text-black p-8 mt-25">
+        <div class="max-w-3xl mx-auto p-4 flex flex-col justify-between font-outfit">
+          <header class="">
+            <h2 class="text-6xl font-outfit font-bold">Random <span class="text-purple-500">Cat Facts</span></h2>
+            <h4 class="text-1xl font-outfit text-gray-600 font-bold">Here's the random <span class="text-purple-500">cat fact</span> of the day!</h4>
+          </header>
+          <div class="m-8 bg-gray-200 rounded-xl">
+            <p>{randFact}</p>
+          </div>
+
+          <header>
+            <h1 class="text-7xl font-outfit font-bold">Let's track some <span class="text-purple-500">Cat Facts</span></h1> 
+          </header>
+          <div class="w-full mt-12 bg-gray-200 rounded-xl">
+            <div class="m-4 p-auto">
+              <h1 class="text-2xl">All Cat Facts</h1>
+            </div>
+            <ul>
+                <table class="table-auto w-full border-collapse">
+                  <thead>
+                    <tr>
+                      <th class="border-t border-gray-400"><label>ID</label></th>
+                      <th class="border-t border-gray-400"><label>Fact</label></th>
+                      <th class="border-t border-gray-400"><label>Created At</label></th>
+                    </tr>
+                  </thead>
         
-          <tbody>
-            {facts.map((fact) =>(
-              <tr key={fact.id}>
-                <td>{fact.id}</td>
-                <td>{fact.fact}</td>
-                <td>{fact.created_at}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </ul>
-      <CatFactForm onFactAdded={fetchFacts}></CatFactForm>
-      <h2>Here's a Random Cat Fact</h2>
-      <p>{randFact}</p>
+                <tbody className="space-y-5">
+                  {facts.map((fact) =>(
+                    <tr key={fact.id}>
+                      <td class="border-t border-gray-400">{fact.id}</td>
+                      <td class="border-t border-gray-400">{fact.fact}</td>
+                      <td class="border-t border-gray-400">{fact.created_at}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </ul>
+          </div>
+
+          <div class="mt-12">
+            <CatFactForm onFactAdded={fetchFacts}></CatFactForm>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- frontend/vite-project/src/CatFactForm.jsx
+++ frontend/vite-project/src/CatFactForm.jsx
@@ -40,17 +40,20 @@ function CatFactForm({ onFactAdded }) {
 
   return (
     <div>
-      <h2>Submit a Cat Fact</h2>
+      <header>
+        <h1 class="text-6xl font-outfit font-bold">Submit a <span class="text-purple-500">Cat Fact</span></h1> 
+      </header>
       {message && <p>{message}</p>}
-      <form onSubmit={handleSubmit}>
+      <form class="flex gap-2 items-center justify-center"onSubmit={handleSubmit}>
         <input
           type="text"
           value={fact}
           onChange={(e) => setFact(e.target.value)}
           placeholder="Type your cat fact here..."
           required
+          class="bg-gray-200 text-2xl"
         />
-        <button type="submit">Submit</button>
+        <button class="bg-purple-500" type="submit">Submit</button>
       </form>
     </div>
   );
